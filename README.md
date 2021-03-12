0c9f5f6094d2ca3b8031a282a25879819540ca87
Carter Landis <carter.landis@gusto.com>
2021-03-12 12:51:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swiftbar Plugins
+
+This repo currently contains my only plugin, `old-docker-containers`. Every 5 minutes, it will set the menu bar item to notify me if I have Docker containers running or not, and if so, if there are any that have been running for longer than 8 hours.
+
+## Config
+- Plugins are stored in `$HOME/.config/swiftbar/`
+- Create a regular symlink for whatever plugin in this directory to the config folder
+
+### old-docker-containers
+
+TODO:
+- [ ] Add fancy formatting?
+- [ ] Use better emoji?
+- [ ] Blank when no output
+- [ ] Add list of exceptions (is user input allowed?)
+
+
---
+++ old-docker-containers.5m.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# <bitbar.title>Long-running Docker containers</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Carter Landis</bitbar.author>
+# <bitbar.author.github>ccarterlandis</bitbar.author.github>
+# <bitbar.desc>Notifies you when you have any Docker containers that have been running for more than 8 hours.</bitbar.desc>
+# <bitbar.image>http://www.hosted-somewhere/pluginimage</bitbar.image>
+# <bitbar.dependencies>python3,docker</bitbar.dependencies>
+
+have_long_running_containers = False
+
+import subprocess
+
+running_containers = [container for container in subprocess.run("docker container ls -a --filter status=running --format '{{.Names}} -- {{.RunningFor}}'", capture_output=True, text=True, shell=True).stdout.split('\n') if container != '']
+
+for container in running_containers:
+    running_for = container.split(' -- ')[1].replace('ago', '').split(' ')
+    # anything that's not seconds or minutes (only options are hours, days, weeks, months, and years), so any value of those over 8 is too long
+    if running_for[1] not in ['seconds', 'minutes'] and int(running_for[0]) > 8:
+        have_long_running_containers = True
+        break
+
+if have_long_running_containers:
+    print("🐋❗️")
+elif not have_long_running_containers and running_containers != []:
+    print("🐋")
+else:
+    print("🐋🚫")
+
+print("---------------")
+if running_containers != []:
+    for container in running_containers:
+        print(container)
+
